f8a064018be114db4149efceffe0aeb0bf5cfc98
hyk3018 <47370661+hyk3018@users.noreply.github.com>
2020-08-30 23:09:33 +0100

    Implemented holding click event that opens up command panel UI and a close button to close the panel

--- Assets/_project/Scripts/Commands/CommandInput.cs
+++ Assets/_project/Scripts/Commands/CommandInput.cs
@@ -8,7 +8,13 @@ namespace FeedTheBaby.Commands
     public class CommandInput : MonoBehaviour
     {
         [SerializeField] LayerMask collisionMask = default;
+        [SerializeField] float minimumHoldDuration = 1f;
         CommandManager _commandManager;
+
+        public Action<Vector3, Transform, HoldType> OnTileHeld;
+
+        float currentHoldDuration;
+        bool holding;
 
         void Awake()
         {
@@ -17,18 +23,46 @@ namespace FeedTheBaby.Commands
 
         void Update()
         {
-            if (Input.GetMouseButtonDown(1))
+            if (Input.GetMouseButton(1))
+            {
+                currentHoldDuration += Time.deltaTime;
+            }
+
+            if (Input.GetMouseButtonUp(1))
             {
                 RaycastHit2D hit = Physics2D.GetRayIntersection(Camera.main.ScreenPointToRay(Input.mousePosition),
                     Single.PositiveInfinity, collisionMask);
                 if (hit.transform == null)
                 {
                     Vector3 groundPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                    _commandManager.AddCommand(new MovePositionCommand(groundPosition));
+                    if (currentHoldDuration < minimumHoldDuration)
+                    {
+                        _commandManager.AddCommand(new MovePositionCommand(groundPosition));
+                    }
+                    else
+                    {
+                        currentHoldDuration = 0;
+                        OnTileHeld(groundPosition, null, HoldType.GROUND);
+                    }
                 }
                 else if (hit.transform.gameObject.layer != LayerMask.NameToLayer("UI"))
-                    _commandManager.AddCommand(new MoveAndInteractCommand(hit.transform));
+                {
+                    if (currentHoldDuration < minimumHoldDuration)
+                    {
+                        _commandManager.AddCommand(new MoveAndInteractCommand(hit.transform));
+                    }
+                    else
+                    {
+                        currentHoldDuration = 0;
+                        OnTileHeld(default, hit.transform, HoldType.LEVEL_OBJECT);
+                    }
+                }
             }
         }
+    }
+
+    public enum HoldType
+    {
+        GROUND, LEVEL_OBJECT
     }
 }
---
+++ Assets/_project/Scripts/UI/Level/CommandPanelUI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FeedTheBaby.Commands;
+using UnityEngine;
+
+namespace FeedTheBaby.UI
+{
+    public class CommandPanelUI : MonoBehaviour
+    {
+        [SerializeField] CommandInput commandInput = null;
+        [SerializeField] GameObject commandPanel = null;
+
+        void Start()
+        {
+            commandInput.OnTileHeld += ShowPanel;
+            commandPanel.SetActive(false);
+        }
+
+        void ShowPanel(Vector3 position, Transform transform, HoldType holdType)
+        {
+            if (holdType == HoldType.GROUND)
+            {
+                commandPanel.SetActive(true);
+                commandPanel.transform.position = new Vector3(position.x, position.y, commandPanel.transform.position.z);
+            }
+        }
+
+        public void HidePanel()
+        {
+            commandPanel.SetActive(false);
+        }
+    }
+}
